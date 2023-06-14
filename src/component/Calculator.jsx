7837ed752df1e3d88da9456fa2d476600546910d
jules-jean-louis1 <jules.jean-louis@laplateforme.io>
2023-06-14 09:11:59 +0200

    Add Title Calculator

---
+++ src/component/Calculator.jsx
@@ -0,0 +1,10 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+
+function Calculator() {
+  return <div>
+        <h1>Calculator9000</h1>
+    </div>;
+}
+
+export default Calculator;
